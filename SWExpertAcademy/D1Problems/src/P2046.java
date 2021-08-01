f8cb45193b4e1a4b93cfc8e759a52a88e3087b2e
Kimyeongjun <dudwns650395@gmail.com>
2021-08-01 15:21:42 +0900

    update: 2046번 스탬프 찍기 문제

---
+++ SWExpertAcademy/D1Problems/src/P2046.java
@@ -0,0 +1,17 @@
+/*
+SW Expert Academy 문제 2046 스탬프 찍기
+*/
+
+import java.util.Scanner;
+
+public class P2046 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        for(int i = 0; i < num; i++) {
+            System.out.print("#");
+        }
+    }
+}
