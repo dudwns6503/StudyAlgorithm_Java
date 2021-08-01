63e54a33bdbf4865824ff7986bcaf9d5b012f99e
Kimyeongjun <dudwns650395@gmail.com>
2021-08-01 16:07:56 +0900

    update: 2027번 대각선 출력하기 문제 풀이

---
+++ SWExpertAcademy/D1Problems/src/P2027.java
@@ -0,0 +1,16 @@
+/*
+SW Expert Academy 문제 2027 대각선 출력하기
+*/
+
+import java.util.Scanner;
+
+public class P2027 {
+
+    public static void main(String[] args) {
+        System.out.println("#++++");
+        System.out.println("+#+++");
+        System.out.println("++#++");
+        System.out.println("+++#+");
+        System.out.println("++++#");
+    }
+}
