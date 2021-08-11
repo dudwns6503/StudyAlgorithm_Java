a90a085e734f6c4926b922f29a1e1c1f22ff6e09
Kimyeongjun <dudwns650395@gmail.com>
2021-08-12 08:54:50 +0900

    update: 1000번 A+B 문제 풀이

---
+++ BaekJoon/B5/src/P1000.java
@@ -0,0 +1,14 @@
+/*
+BaekJoon 문제 1000 A+B
+*/
+
+import java.util.Scanner;
+
+public class P1000 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        System.out.println(num1 + num2);
+    }
+}
