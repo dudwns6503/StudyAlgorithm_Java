4c9a16c2e03a79ef5ed80aaa53b6edfb30545b5f
Kimyeongjun <dudwns650395@gmail.com>
2021-08-12 08:56:00 +0900

    update: 1001번 A-B 문제 풀이

---
+++ BaekJoon/B5/src/P1001.java
@@ -0,0 +1,14 @@
+/*
+BaekJoon 문제 1001 A-B
+*/
+
+import java.util.Scanner;
+
+public class P1001 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        System.out.println(num1 - num2);
+    }
+}
