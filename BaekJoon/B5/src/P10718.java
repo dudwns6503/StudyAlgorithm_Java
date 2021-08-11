68e90e9763121982703568162cdc9bb2c192aeb0
Kimyeongjun <dudwns650395@gmail.com>
2021-08-12 08:47:51 +0900

    update: 10718번 We love kriii 문제 풀이

---
+++ BaekJoon/B5/src/P10718.java
@@ -0,0 +1,11 @@
+/*
+BaekJoon 문제 10718 We love kriii
+*/
+
+public class P10718 {
+
+    public static void main(String[] args) {
+        System.out.println("강한친구 대한육군");
+        System.out.print("강한친구 대한육군");
+    }
+}
