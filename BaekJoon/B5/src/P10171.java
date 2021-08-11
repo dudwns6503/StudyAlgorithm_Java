1f9ef413c224fe9ef94f2725aab00323d27e3293
Kimyeongjun <dudwns650395@gmail.com>
2021-08-12 08:50:17 +0900

    update: 10171번 고양이 문제 풀이

---
+++ BaekJoon/B5/src/P10171.java
@@ -0,0 +1,13 @@
+/*
+BaekJoon 문제 10171 고양이 문제
+*/
+
+public class P10171 {
+
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.print(" \\(__)|");
+    }
+}
