64b9ff317bd67b6cb223f1f3f648d4dee35076c6
Kimyeongjun <dudwns650395@gmail.com>
2021-08-13 07:46:10 +0900

    update: 8393번 합 문제 풀이

---
+++ BaekJoon/B3/src/P8393.java
@@ -0,0 +1,17 @@
+/*
+BaekJoon 8393 합
+*/
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class P8393 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int sum = 0;
+        for(int i=1; i<=n; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
