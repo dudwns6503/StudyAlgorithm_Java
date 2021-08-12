a402d82693400c33ca8c31cbe8f6ece305e8a5a5
Kimyeongjun <dudwns650395@gmail.com>
2021-08-13 07:07:11 +0900

    update: 1330번 두 수 비교하기 문제 풀이

---
+++ BaekJoon/B4/src/P1330.java
@@ -0,0 +1,18 @@
+/*
+BaekJoon 1330 두 수 비교하기
+*/
+
+import java.util.Scanner;
+
+public class P1330 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if (a > b) System.out.println(">");
+        else if (a < b) System.out.println("<");
+        else System.out.println("==");
+    }
+}
