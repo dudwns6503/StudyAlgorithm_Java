55ff248b9d5c1eac77e671e3b818da9423f9ec47
Kimyeongjun <dudwns650395@gmail.com>
2021-08-13 15:23:38 +0900

    update: 10951번 A+B-4 문제 풀이

---
+++ BaekJoon/B3/src/P10951.java
@@ -0,0 +1,18 @@
+/*
+BaekJoon 10951 A+B-4
+*/
+import java.util.Scanner;
+
+public class P10951 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while(sc.hasNext()) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(a + b);
+        }
+        sc.close();
+    }
+}
