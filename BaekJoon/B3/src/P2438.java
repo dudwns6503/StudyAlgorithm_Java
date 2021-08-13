353cdbbe584c847bcfc621d444aa399276571406
Kimyeongjun <dudwns650395@gmail.com>
2021-08-13 12:05:09 +0900

    update: 2438번 별찍기-1 문제 풀이

---
+++ BaekJoon/B3/src/P2438.java
@@ -0,0 +1,17 @@
+/*
+BaekJoon 2438 별찍기-1
+*/
+import java.util.Scanner;
+
+public class P2438 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<=i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
