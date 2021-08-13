ecac5b64b2a178e8dfadb5f407b6313e4f1807a1
Kimyeongjun <dudwns650395@gmail.com>
2021-08-13 15:17:15 +0900

    update: 10952번 A+B-5 문제 풀이

---
+++ BaekJoon/B3/src/P10952.java
@@ -0,0 +1,20 @@
+/*
+BaekJoon 10952 A+B-5
+*/
+import java.util.Scanner;
+
+public class P10952 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = -1;
+        int b = -1;
+        while(true) {
+            a = sc.nextInt();
+            b = sc.nextInt();
+            if(a == 0 && b == 0)
+                break;
+            System.out.println(a + b);
+        }
+    }
+}
