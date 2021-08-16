ce59a77f48fe1fa901ed627fcf8449288340b85e
Kimyeongjun <dudwns650395@gmail.com>
2021-08-16 18:52:32 +0900

    update: 11654번 아스키 코드 문제 풀이

---
+++ BaekJoon/B5/src/P11654.java
@@ -0,0 +1,14 @@
+/*
+BaekJoon 11654 아스키 코드
+*/
+
+import java.util.*;
+
+public class P11654 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        char chr = str.charAt(0);
+        System.out.println((int) chr);
+    }
+}
