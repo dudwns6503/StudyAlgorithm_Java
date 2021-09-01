d84e380c0aa56d182c120a5fb8205b4211de4666
Kimyeongjun <dudwns650395@gmail.com>
2021-09-01 09:07:30 +0900

    update: 2558번 A+B - 2 문제 풀이

---
+++ BaekJoon/B5/src/P2558.java
@@ -0,0 +1,16 @@
+/*
+BaekJoon 2558 A+B - 2
+*/
+
+import java.util.*;
+
+public class P2558 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        System.out.println(A+B);
+    }
+}
