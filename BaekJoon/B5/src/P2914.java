e44721fd961315b7bc246f0ed39f46ffe301a13d
Kimyeongjun <dudwns650395@gmail.com>
2021-09-03 03:46:46 +0900

    update: 2914번 저작권 문제 풀이

---
+++ BaekJoon/B5/src/P2914.java
@@ -0,0 +1,16 @@
+/*
+BaekJoon 2914 저작권
+*/
+
+import java.util.*;
+
+public class P2914 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int I = sc.nextInt();
+
+        System.out.println(A * (I - 1) + 1);
+    }
+}
