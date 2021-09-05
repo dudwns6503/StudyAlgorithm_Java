2354d28c115acdf71522c9c9d694a9d5d54e2f94
Kimyeongjun <dudwns650395@gmail.com>
2021-09-05 23:43:41 +0900

    update: 3046번 R2 문제 풀이

---
+++ BaekJoon/B5/src/P3046.java
@@ -0,0 +1,15 @@
+/*
+BaekJoon 3046 R2
+*/
+
+import java.util.*;
+
+public class P3046 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(b * 2 - a);
+    }
+}
