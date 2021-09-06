8003fd4d2722b9271510b0d01ae6aa6383ff2f8c
Kimyeongjun <dudwns650395@gmail.com>
2021-09-06 23:20:29 +0900

    update: 14645번 와이버스 부릉부릉 문제 풀이

---
+++ BaekJoon/B5/src/P14645.java
@@ -0,0 +1,19 @@
+/*
+BaekJoon 14645 와이버스 부릉부릉
+*/
+
+import java.util.*;
+
+public class P14645 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        for(int i=0; i<N; i++) {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+        }
+        System.out.println("비와이");
+    }
+}
