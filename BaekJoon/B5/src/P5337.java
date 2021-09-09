6081cc4e7de36ee2e2744c9aa83817ff7ce0daeb
Kimyeongjun <dudwns650395@gmail.com>
2021-09-09 23:59:21 +0900

    update: 5337번 웰컴 문제 풀이

---
+++ BaekJoon/B5/src/P5337.java
@@ -0,0 +1,11 @@
+/*
+BaekJoon 2480 주사위 세개
+
+예외처리를 확실하게 하는 것이 포인트이다. -> 반례를 생각하는데 시간이 오래걸림.
+*/
+public class P5337 {
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
