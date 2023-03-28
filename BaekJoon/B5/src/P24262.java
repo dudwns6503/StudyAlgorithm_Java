9d1a855ae2127bc0d8dc69a05372aa2b86c8f6fb
Kimyeongjun <dudwns650395@gmail.com>
2023-03-28 15:50:15 +0900

    24262 알고리즘 수업 - 알고리즘의 수행 시간 1
    구현 + 시뮬레이션.

---
+++ BaekJoon/B5/src/P24262.java
@@ -0,0 +1,26 @@
+/*
+ * BOJ 24262 알고리즘 수업 - 알고리즘의 수행 시간 1
+ *
+ * memory : 14132kb
+ * time : 128ms
+ */
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class P24262 {
+
+    public static void main(String[] args) throws Exception {
+        input();
+    }
+
+    private static void input() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.println("1");
+        System.out.print("0");
+    }
+}
