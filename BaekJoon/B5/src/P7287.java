9c71fd246160254d2f7d4481383ef4836f429afe
yeongjun <dudwns650395@gmail.com>
2023-04-12 20:56:34 +0900

    7287 등록
    구현.

---
+++ BaekJoon/B5/src/P7287.java
@@ -0,0 +1,15 @@
+ /*
+  * BOJ 7287 등록
+  *
+  * memory : 13944kb
+  * time : 112ms
+  */
+
+
+public class P7287 {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(361);
+        System.out.print("dudwns6503");
+    }
+}
