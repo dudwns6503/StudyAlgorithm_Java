c8bf2197c9c232425f6df038c8729b9e3bc78b25
yeongjun <dudwns650395@gmail.com>
2023-05-22 23:42:04 +0900

    5339 콜센터
    구현.

---
+++ BaekJoon/B5/src/P5339.java
@@ -0,0 +1,32 @@
+/*
+ * BOJ 5339 콜센터
+ *
+ * memory : 13940kb
+ * time : 112ms
+ */
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class P5339 {
+
+    public static void main(String[] args) throws Exception {
+        input();
+    }
+
+    private static void input() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("     /~\\");
+        System.out.println("    ( oo|");
+        System.out.println("    _\\=/_");
+        System.out.println("   /  _  \\");
+        System.out.println("  //|/.\\|\\\\");
+        System.out.println(" ||  \\ /  ||");
+        System.out.println("============");
+        System.out.println("|          |");
+        System.out.println("|          |");
+        System.out.print("|          |");
+    }
+}
