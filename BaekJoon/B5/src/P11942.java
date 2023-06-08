87c8dd9c23825dd1a9fc84e2309e5487c40533af
yeongjun <dudwns650395@gmail.com>
2023-06-08 21:08:36 +0900

    11942 고려대는 사랑입니다

---
+++ BaekJoon/B5/src/P11942.java
@@ -0,0 +1,14 @@
+/*
+ * BOJ 11942 고려대는 사랑입니다
+ *
+ * memory : 13956kb
+ * time : 116ms
+ */
+
+
+public class P11942 {
+
+    public static void main(String[] args) {
+        System.out.print("고려대학교");
+    }
+}
