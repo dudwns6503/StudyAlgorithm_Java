260faa202c35266c745edb2050eb0d305f45f8a7
Kimyeongjun <dudwns650395@gmail.com>
2023-06-23 14:20:51 +0900

    15733 나는 누구인가
    출력

---
+++ BaekJoon/B5/src/P15733.java
@@ -0,0 +1,14 @@
+/*
+ * BOJ 15733 나는 누구인가
+ *
+ * memory : 13940kb
+ * time : 116ms
+ */
+
+
+public class P15733 {
+
+    public static void main(String[] args) {
+        System.out.print("I\'m Sexy");
+    }
+}
