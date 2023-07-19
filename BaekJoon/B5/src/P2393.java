74df3135dcf28e1d648faa455e35a3548019b942
yeongjun <dudwns650395@gmail.com>
2023-07-19 23:34:14 +0900

    2393 Rook
    구현

---
+++ BaekJoon/B5/src/P2393.java
@@ -0,0 +1,26 @@
+/*
+ * BOJ 2393 Rook
+ *
+ * memory : 14080kb
+ * time : 116ms
+ */
+
+
+public class P2393 {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("  ___  ___  ___\n" +
+                "  | |__| |__| |\n" +
+                "  |           |\n" +
+                "   \\_________/\n" +
+                "    \\_______/\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |_____|\n" +
+                "  __/       \\__\n" +
+                " /             \\\n" +
+                "/_______________\\");
+    }
+}
