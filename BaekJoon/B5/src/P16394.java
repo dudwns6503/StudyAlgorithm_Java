57513b2c115fa732d7a589d0816fcc6bbbbb549d
yeongjun <dudwns650395@gmail.com>
2023-07-21 22:51:05 +0900

    16394 홍익대학교
    구현

---
+++ BaekJoon/B5/src/P16394.java
@@ -0,0 +1,24 @@
+/*
+ * BOJ 16394 홍익대학교
+ *
+ * memory : 17592kb
+ * time : 204ms
+ */
+
+
+import java.util.Scanner;
+
+public class P16394 {
+
+    public static void main(String[] args) {
+        input();
+    }
+
+    private static void input() {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        System.out.print(n - 1946);
+    }
+}
