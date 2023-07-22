bab70967009ed871eccb3ee2004b7c3faa5db5d5
yeongjun <dudwns650395@gmail.com>
2023-07-22 23:36:31 +0900

    5338 마이크로소프트 로고
    구현

---
+++ BaekJoon/B5/src/P5338.java
@@ -0,0 +1,18 @@
+/*
+ * BOJ 5338 마이크로소프트 로고
+ *
+ * memory : 13864kb
+ * time : 120ms
+ */
+
+
+public class P5338 {
+
+    public static void main(String[] args) throws Exception {
+        System.out.print("       _.-;;-._\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-;;-._|\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-''-._|");
+    }
+}
