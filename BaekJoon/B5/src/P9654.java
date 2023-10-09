cbb9bae41341bae6877bb6beabb0b0965c16ecfb
yeongjun <dudwns650395@gmail.com>
2023-10-09 22:27:21 +0900

    9654 나부 함대 데이터
    문자열 출력

---
+++ BaekJoon/B5/src/P9654.java
@@ -0,0 +1,23 @@
+/*
+ * BOJ 9654 나부 함대 데이터
+ *
+ * memory : 13856kb
+ * time : 112ms
+ */
+
+
+public class P9654 {
+
+    public static void main(String[] args) throws Exception {
+        input();
+    }
+
+    private static void input() throws Exception {
+        System.out.println("SHIP NAME      CLASS          DEPLOYMENT IN SERVICE\n" +
+                "N2 Bomber      Heavy Fighter  Limited    21        \n" +
+                "J-Type 327     Light Combat   Unlimited  1         \n" +
+                "NX Cruiser     Medium Fighter Limited    18        \n" +
+                "N1 Starfighter Medium Fighter Unlimited  25        \n" +
+                "Royal Cruiser  Light Combat   Limited    4");
+    }
+}
