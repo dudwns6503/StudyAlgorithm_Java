e7cfc99e7e1eaee20606948bc468e7317c89374d
dudwns6503 <dudwns650395@gmail.com>
2023-10-12 08:42:35 +0900

    10189 Hook
    문자열 출력

---
+++ BaekJoon/B5/src/P10189.java
@@ -0,0 +1,17 @@
+/*
+ * BOJ 10189 Hook
+ *
+ * memory : 14096kb
+ * time : 120ms
+ */
+
+
+public class P10189 {
+
+    public static void main(String[] args) {
+        System.out.println("#  # #### #### #  #\n" +
+                "#### #  # #  # # #\n" +
+                "#### #  # #  # # #\n" +
+                "#  # #### #### #  #");
+    }
+}
