3ff546213410b88d504cc84d2cb51577040de57e
yeongjun <dudwns650395@gmail.com>
2023-10-30 20:52:38 +0900

    23234 The World Responds
    출력

---
+++ BaekJoon/B5/src/P23234.java
@@ -0,0 +1,20 @@
+/*
+ * BOJ 23234 The World Responds
+ *
+ * memory : 13940kb
+ * time : 116ms
+ */
+
+
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+
+public class P23234 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("The world says hello!");
+        bw.close();
+    }
+}
