d95e1361b5a98ad3cd870a2b488b550147015888
dudwns6503 <dudwns650395@gmail.com>
2023-11-13 09:49:20 +0900

    18096 Арифметическая магия
    수학

---
+++ BaekJoon/B5/src/P18096.java
@@ -0,0 +1,25 @@
+/*
+ * BOJ 18096 Арифметическая магия
+ *
+ * memory : 14136kb
+ * time : 120ms
+ */
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class P18096 {
+
+    public static void main(String[] args) throws Exception {
+        input();
+    }
+
+    private static void input() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.print("1");
+    }
+}
