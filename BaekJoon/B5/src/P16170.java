984d21a5411d9e68723b3f873454999ab68e810f
yeongjun <dudwns650395@gmail.com>
2023-11-16 20:26:00 +0900

    16170 오늘의 날짜는?
    단순 출력

---
+++ BaekJoon/B5/src/P16170.java
@@ -0,0 +1,16 @@
+/*
+ * BOJ 16170 오늘의 날짜는?
+ *
+ * memory : 13864kb
+ * time : 120ms
+ */
+
+
+public class P16170 {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("2023");
+        System.out.println("11");
+        System.out.print("16");
+    }
+}
