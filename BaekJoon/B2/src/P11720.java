da39957394c2dba783e071c14512885dbc0f8769
dudwns6503 <dudwns650395@gmail.com>
2024-07-12 14:58:43 +0900

    11720 숫자의 합
    문자열

--- BaekJoon/B2/src/P11720.java
+++ BaekJoon/B2/src/P11720.java
@@ -1,24 +1,28 @@
 /*
-BaekJoon 11720 숫자의 합
-*/
+ * BOJ 11720 숫자의 합
+ *
+ * memory : 14316kb
+ * time : 104ms
+ */
+
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class P11720 {
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        String str;
-        String[] strArray = new String[N];
+
+        int n = Integer.parseInt(br.readLine());
+
+        String number = br.readLine();
+
         int sum = 0;
-        str = br.readLine();
-        strArray = str.split("");
-
-        for(int i=0; i<N; i++) {
-            sum += Integer.parseInt(strArray[i]);
+        for (char c : number.toCharArray()) {
+            sum += c - '0';
         }
-        System.out.println(sum);
 
+        System.out.print(sum);
     }
 }
