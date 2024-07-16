805a3c757b3f0fd4241c18cfcbe95880110ef575
yeongjun <dudwns650395@gmail.com>
2024-07-17 00:15:55 +0900

    12891 DNA 비밀번호
    슬라이딩 윈도우

--- BaekJoon/S4/src/P12891.java
+++ BaekJoon/S4/src/P12891.java
@@ -1,92 +1,90 @@
 /*
  * BOJ 12891 DNA 비밀번호
  *
- * memory : 21296kb
- * time : 348ms
+ * memory : 46020kb
+ * time : 492ms
  */
 
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 public class P12891 {
 
-    private static int[] rules, myRules;
-    private static String strings;
-    private static int s, p, checkSecret, ans;
+    private static String str;
+    private static Map<Character, Integer> rules;
+    private static Map<Character, Integer> myRules;
 
     public static void main(String[] args) throws Exception {
-        input();
-        solve();
-    }
-
-    private static void input() throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        s = Integer.parseInt(st.nextToken());
-        p = Integer.parseInt(st.nextToken());
+        int s = Integer.parseInt(st.nextToken());
+        int p = Integer.parseInt(st.nextToken());
+
+        str = br.readLine();
+
+        rules = new HashMap<>();
+        myRules = new HashMap<>();
 
         st = new StringTokenizer(br.readLine());
-        strings = st.nextToken();
+        rules.put('A', Integer.parseInt(st.nextToken()));
+        rules.put('C', Integer.parseInt(st.nextToken()));
+        rules.put('G', Integer.parseInt(st.nextToken()));
+        rules.put('T', Integer.parseInt(st.nextToken()));
 
-        rules = new int[4];
-        myRules = new int[4];
+        myRules.put('A', 0);
+        myRules.put('C', 0);
+        myRules.put('G', 0);
+        myRules.put('T', 0);
 
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < 4; i++) {
-            rules[i] = Integer.parseInt(st.nextToken());
-            if (rules[i] == 0) checkSecret++;
-        }
-    }
-
-    private static void solve() {
         for (int i = 0; i < p; i++) {
-            check(strings.charAt(i));
+            char c = str.charAt(i);
+
+            if (myRules.containsKey(c))
+                myRules.put(c, myRules.get(c) + 1);
+            else
+                myRules.put(c, 1);
         }
 
-        if (checkSecret == 4) ans++;
+        int ans = 0;
+        if (check())
+            ans++;
 
-        for (int i = p; i < s; i++) {
-            int j = i - p;
-            check(strings.charAt(i));
-            checkPrevious(strings.charAt(j));
-            if (checkSecret == 4) ans++;
+        int endIndex = p;
+        for (int i = 0; i < s - p; i++) {
+            char firstCharacter = str.charAt(i);
+
+            myRules.put(firstCharacter, myRules.get(firstCharacter) - 1);
+
+            char lastCharacter = str.charAt(endIndex);
+
+            myRules.put(lastCharacter, myRules.get(lastCharacter) + 1);
+
+            if (check())
+                ans++;
+
+            endIndex++;
         }
 
         System.out.print(ans);
     }
 
-    private static void check(char ch) {
-        if (ch == 'A') {
-            myRules[0]++;
-            if (myRules[0] == rules[0]) checkSecret++;
-        } else if (ch == 'C') {
-            myRules[1]++;
-            if (myRules[1] == rules[1]) checkSecret++;
-        } else if (ch == 'G') {
-            myRules[2]++;
-            if (myRules[2] == rules[2]) checkSecret++;
-        } else if (ch == 'T'){
-            myRules[3]++;
-            if (myRules[3] == rules[3]) checkSecret++;
-        }
-    }
+    private static boolean check() {
+        int cnt = 0;
 
-    private static void checkPrevious(char ch) {
-        if (ch == 'A') {
-            if (myRules[0] == rules[0]) checkSecret--;
-            myRules[0]--;
-        } else if (ch == 'C') {
-            if (myRules[1] == rules[1]) checkSecret--;
-            myRules[1]--;
-        } else if (ch == 'G') {
-            if (myRules[2] == rules[2]) checkSecret--;
-            myRules[2]--;
-        } else if (ch == 'T'){
-            if (myRules[3] == rules[3]) checkSecret--;
-            myRules[3]--;
-        }
+        if (myRules.get('A') >= rules.get('A'))
+            cnt++;
+        if (myRules.get('C') >= rules.get('C'))
+            cnt++;
+        if (myRules.get('G') >= rules.get('G'))
+            cnt++;
+        if (myRules.get('T') >= rules.get('T'))
+            cnt++;
+
+        return cnt == 4;
     }
 }
