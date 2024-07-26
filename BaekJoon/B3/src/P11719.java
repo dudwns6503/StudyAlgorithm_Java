7a849888e9cd04146a1b08776143ac2ad0393039
yeongjun Kim <dudwns650395@gmail.com>
2024-07-26 16:38:37 +0900

    11719 그대로 출력하기 2
    입출력

---
+++ BaekJoon/B3/src/P11719.java
@@ -0,0 +1,22 @@
+/*
+ * BOJ 11719 그대로 출력하기 2
+ *
+ * memory : 14452kb
+ * time : 132ms
+ */
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class P11719 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s;
+        while ((s = br.readLine()) != null) {
+            System.out.println(s);
+        }
+    }
+}
