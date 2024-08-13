b662926746a635e3d0c6409786a322efce540e00
yeongjun Kim <dudwns650395@gmail.com>
2024-08-13 16:31:46 +0900

    10430 나머지
    수학

--- BaekJoon/B5/src/P10430.java
+++ BaekJoon/B5/src/P10430.java
@@ -1,20 +1,37 @@
 /*
-BaekJoon 10430 나머지
-*/
+ * BOJ 10430 나머지
+ *
+ * memory : 14160kb
+ * time : 120ms
+ */
 
-import java.util.Scanner;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class P10430 {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
+    private static int a, b, c;
 
-        System.out.println((a+b)%c);
-        System.out.println(((a%c) + (b%c))%c);
-        System.out.println((a*b)%c);
-        System.out.println(((a%c)*(b%c))%c);
+    public static void main(String[] args) throws Exception {
+        input();
+        solve();
+    }
+    
+    private static void solve() {
+        System.out.println((a + b) % c);
+        System.out.println(((a % c) + (b % c)) % c);
+        System.out.println((a * b) % c);
+        System.out.print(((a % c) * (b % c)) % c);
+    }
+    
+    private static void input() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        a = Integer.parseInt(st.nextToken());
+        b = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
     }
 }
