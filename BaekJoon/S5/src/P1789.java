c6b96f188c7ebed9a915d9563363040a229658d4
yeongjun Kim <dudwns650395@gmail.com>
2024-10-28 13:18:02 +0900

    (Re)1789 수들의 합
    그리디

--- BaekJoon/S5/src/P1789.java
+++ BaekJoon/S5/src/P1789.java
@@ -1,37 +1,42 @@
 /*
  * BOJ 1789 수들의 합
  *
- * memory : 17672kb
- * time : 212ms
+ * memory : 14252kb
+ * time : 108ms
  */
 
-import java.util.Scanner;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class P1789 {
 
-    static long S, sum;
+    private static long s;
 
-    static void input() {
-        Scanner sc = new Scanner(System.in);
-        S = sc.nextLong();
+    public static void main(String[] args) throws Exception {
+        input();
+        solve();
     }
 
-    static void sol() {
-        for(int i=1; i<=S; i++) {
+    private static void solve() {
+        long sum = 0;
+        for (long i = 1; i <= s; i++) {
             sum += i;
-            if(S == 1) {
-                System.out.println(1);
+
+            if (s == 1) {
+                System.out.print(1);
                 break;
             }
-            else if(sum > S) {
-                System.out.println(i-1);
+            else if (sum > s){
+                System.out.print(i - 1);
                 break;
             }
         }
     }
 
-    public static void main(String[] args) {
-        input();
-        sol();
+    private static void input() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        s = Long.parseLong(br.readLine());
     }
 }
