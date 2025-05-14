90e3f2dd0c3310d901f3e3d243911334d4684c04
yeongjun Kim <dudwns650395@gmail.com>
2025-05-15 08:33:41 +0900

    33701 새천년관
    해 구성하기
    =====
    설명 생략

---
+++ BaekJoon/B4/src/P33701.java
@@ -0,0 +1,18 @@
+/*
+ * BOJ 33701 새천년관
+ *
+ * memory : 13900kb
+ * time : 104ms
+ */
+
+
+public class P33701 {
+    
+    public static void main(String[] args) throws Exception {
+        solve();
+    }
+    
+    private static void solve() {
+        System.out.print("nkugwan");
+    }
+}
